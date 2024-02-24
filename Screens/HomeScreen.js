b23ea467c52add96823b02a515fdd06524e39204
Diana de Oliveira Martins <ddom.snf21@uea.edu.br>
2024-02-24 15:05:21 -0400

    Menu button

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,8 +1,14 @@
-import { View, Text, Button, StyleSheet, ProgressBarAndroid, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, ProgressBarAndroid } from 'react-native';
+import { Menu, Divider, Provider } from 'react-native-paper';
 import MonthsList from '../Components/Home/MonthsList';
 
 const HomeScreen = ({ navigation }) => {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
 
   var totalSpent = 320, maxToSpent = 1000;
   const progress = (totalSpent / maxToSpent) * 100;
@@ -12,40 +18,57 @@ const HomeScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header_container}>
-        <Text style={styles.header_text}>Você está em</Text>
-        <Text style={styles.current_month}>DEZEMBRO/2024</Text>
-        <Text style={styles.header_text}>Meta de gastos do mês</Text>
-
-        <View style={styles.money_spent_container}>
-          <ProgressBarAndroid
-            style={styles.progress_bar}
-            styleAttr="Horizontal"
-            indeterminate={false}
-            color={'#B246F2'}
-            progress={progress / 100}
-          />
-          <View style={styles.money_spent_texts_view}>
-            <Text style={styles.header_text}>R${totalSpent}</Text>
-            <Text style={styles.header_text}>R${maxToSpent}</Text>
+    <Provider>
+      <View style={styles.container}>
+      <View style={styles.menu_container}>
+                  <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={<Button onPress={openMenu} title="Menu" color="#30024A"/>}>
+            <Menu.Item onPress={() => {}} title="Registrar gasto" />
+            <Divider />
+            <Menu.Item onPress={() => {}} title="Criar categoria" />
+            <Divider />
+            <Menu.Item onPress={() => {}} title="Reportar gastos" />
+          </Menu>
           </View>
-        </View>
+        <View style={styles.header_container}>
+          <Text style={styles.header_text}>Você está em</Text>
+          <Text style={styles.current_month}>DEZEMBRO/2024</Text>
+          <Text style={styles.header_text}>Meta de gastos do mês</Text>
+
+          <View style={styles.money_spent_container}>
+            <ProgressBarAndroid
+              style={styles.progress_bar}
+              styleAttr="Horizontal"
+              indeterminate={false}
+              color={'#B246F2'}
+              progress={progress / 100}
+            />
+            <View style={styles.money_spent_texts_view}>
+              <Text style={styles.header_text}>R${totalSpent}</Text>
+              <Text style={styles.header_text}>R${maxToSpent}</Text>
+            </View>
+          </View>
         </View>
 
         <View style={{ flex: 1, width: '100%'}}>
           <Text style={styles.month_list_text}>Despesas do ano</Text>
           <MonthsList handleMonthPress={handleMonthPress} />
         </View>
-        
-    </View>
-  )
-}
+      </View>
+    </Provider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  menu_container: {
+    backgroundColor: '#F3D9FF',
+    alignItems: 'flex-start',
+    marginLeft: 10,
   },
   header_container: {
     paddingTop: 30,
